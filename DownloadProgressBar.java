ae177d4e26170edbadf9edfa1f8ef95b13411183
esp2022 <114547099+esp2022@users.noreply.github.com>
2022-12-08 18:10:01 -0500

    Update DownloadProgressBar.java

--- DownloadProgressBar.java
+++ DownloadProgressBar.java
@@ -1,1 +1,13 @@
+package cs1302.gallery;
 
+import javafx.scene.control.ProgressBar;
+
+/** Establishes progress bar. */
+public class DownloadProgressBar extends ProgressBar {
+
+/** Contructor. */
+    DownloadProgressBar() {
+
+    }
+
+}
