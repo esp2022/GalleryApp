de83ae798cfdfe5af873d793dc1d7b321607cf4a
esp2022 <114547099+esp2022@users.noreply.github.com>
2022-12-09 17:56:03 -0500

    Create SearchText.java

---
+++ SearchText.java
@@ -0,0 +1,14 @@
+package cs1302.gallery;
+
+import javafx.scene.control.TextField;
+
+/** Establishes search text. */
+public class SearchText extends TextField {
+
+    /** Contructor. */
+    SearchText() {
+        setText("daft punk");
+
+    }
+
+}
