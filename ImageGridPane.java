a2edb42467ca5e9a22964ef2cc48d84b8c386d58
esp2022 <114547099+esp2022@users.noreply.github.com>
2022-12-09 17:48:58 -0500

    Create ImageGridPane.java

---
+++ ImageGridPane.java
@@ -0,0 +1,13 @@
+package cs1302.gallery;
+
+import javafx.scene.layout.GridPane;
+
+/** Establishes image grid pane. */
+public class ImageGridPane extends GridPane {
+
+    /** Constructor. */
+    ImageGridPane() {
+
+    }
+
+}
