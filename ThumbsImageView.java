8edb7bb157f02624510e4330e184a5b2eb3a51dd
esp2022 <114547099+esp2022@users.noreply.github.com>
2022-12-09 17:59:24 -0500

    Create ThumbsImageView.java

---
+++ ThumbsImageView.java
@@ -0,0 +1,21 @@
+package cs1302.gallery;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/** Establishes thumbs image view. */
+public class ThumbsImageView extends ImageView {
+
+    /**Constructor. */
+    ThumbsImageView() {
+
+    }
+
+    /** Constructor two.                                                                                                                                                                                                                      
+     @param image                                                                                                                                                                                                                             
+    */
+    ThumbsImageView(Image image) {
+        super(image);
+    }
+
+}
