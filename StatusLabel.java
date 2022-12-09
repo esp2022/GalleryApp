9ea67a27d4cf586f0e3fc46830b9d4d3bb9bf00a
esp2022 <114547099+esp2022@users.noreply.github.com>
2022-12-09 17:58:17 -0500

    Create StatusLabel.java

---
+++ StatusLabel.java
@@ -0,0 +1,12 @@
+package cs1302.gallery;
+
+import javafx.scene.control.Label;
+
+/** Establishes status label. */
+public class StatusLabel extends Label {
+
+/** Constructor. */
+    StatusLabel() {
+        setText("Type in a term, select a media type, then click the button.");
+    }
+}
