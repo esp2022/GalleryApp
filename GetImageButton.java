aaf994f0628d724220f44f3a6f59c0ad01be28e1
esp2022 <114547099+esp2022@users.noreply.github.com>
2022-12-09 17:47:23 -0500

    Create GetImageButton.java

---
+++ GetImageButton.java
@@ -0,0 +1,13 @@
+package cs1302.gallery;
+
+import javafx.scene.control.Button;
+
+/** Establishes get image button. */
+public class GetImageButton extends Button {
+
+    /** Constructor. */
+    GetImageButton() {
+        setText("Get Images");
+    }
+
+}
